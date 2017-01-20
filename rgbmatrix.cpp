eddcaa6a365d41ec8a4c7af5d0e86e9ebd1ca316
yus <a@b.c>
2017-01-20 14:10:43 +0800

    inherit Adafruit_GFX class

--- rgbmatrix.cpp
+++ rgbmatrix.cpp
@@ -1,8 +1,13 @@
 #include "rgbmatrix.h"
 
 RGBMatrix::RGBMatrix(uint8_t w, uint8_t h)
-  : NeoPixelBus((uint16_t)(w*h)), _topo(w,h)
+  : Adafruit_GFX(w, h), NeoPixelBus((uint16_t)(w*h)), _topo(w,h)
 {
   
+}
+
+void RGBMatrix::drawPixel(int16_t x, int16_t y, uint16_t color)
+{
+  SetPixelColor(_topo.Map(x,y), color);
 }
 
--- rgbmatrix.h
+++ rgbmatrix.h
@@ -9,10 +9,12 @@
 #include <NeoPixelBus.h>
 
 // on esp8266 pin GPIO2
-class RGBMatrix : public NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod>
+class RGBMatrix : public Adafruit_GFX, public NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod>
 {
 public:
   RGBMatrix(uint8_t w, uint8_t h);
+  virtual void drawPixel(int16_t x, int16_t y, uint16_t color);
+
 private:
   NeoTopology<RowMajorAlternatingLayout> _topo;
 };
