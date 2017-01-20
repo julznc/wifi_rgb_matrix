3867082b3e6743aae1de49393bb4d9ec7258b672
yus <a@b.c>
2017-01-20 13:13:52 +0800

    led matrix initialization

--- wifi_rgb_matrix.ino
+++ wifi_rgb_matrix.ino
@@ -1,5 +1,28 @@
+/*
+ * WS2812 RGB panel controlled by ESP8266 module
+ */
+#include <NeoPixelAnimator.h>
+#include <NeoPixelBrightnessBus.h>
+#include <NeoPixelBus.h>
+
+// 8x36 matrix
+const uint8_t PanelWidth = 36;
+const uint8_t PanelHeight = 8;
+const uint16_t PixelCount = PanelWidth * PanelHeight;
+NeoTopology<RowMajorAlternatingLayout> topo(PanelWidth, PanelHeight);
+
+// on esp8266 pin GPIO2
+NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod> strip(PixelCount);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while (!Serial); // wait for serial attach
+  Serial.println("\nInitializing...");
+  
+  strip.Begin();
+  strip.Show();
+
+  Serial.println("\nRunning...");
 
 }
 
