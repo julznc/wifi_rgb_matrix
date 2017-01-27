0f748a8d66abfcc2985a2286a5b9f8c6655e56b7
yus <a@b.c>
2017-01-27 11:45:01 +0800

    update index.html

--- webserver.cpp
+++ webserver.cpp
@@ -12,26 +12,27 @@ static RGBMatrix *_display = NULL;
 static char _ready = 0;
 static IPAddress _ip;
 
-static const char *indexhtml = ""
-"<html>"
+static const char *indexhtml = "<html>"
 "<head>"
 "<title>"
 "WiFi RGB"
 "</title>"
 "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
 "</head>"
+"<style media=\"screen\" type=\"text/css\">"
+".formbox{border:2px #CCC solid;padding:0 1em;margin:1em;}"
+".textbox{width:75%;}"
+"</style>"
 "<body>"
-"<h1>WiFi RGB Matrix</h1>"
-"<form action=\"/text\" method=\"post\">"
-"<p>Text:"
-"<input type=\"text\" name=\"text\" required></input><br/>"
-"Color:"
-"<input type=\"color\" name=\"color\" value=\"#ffffff\"></input><br/>"
+"<h2>WiFi RGB Matrix</h2>"
+"<form action=\"/text\" method=\"post\" class=\"formbox\">"
+"<p>Text: <input type=\"text\" name=\"text\" maxlength=\"256\" class=\"textbox\"></input><br/>"
+"Color: <input type=\"color\" name=\"color\" value=\"#ffffff\"></input><br/>"
 "<button type=\"submit\">Set</button></p>"
 "</form>"
-"<form action=\"/brightness\" method=\"post\">"
+"<form action=\"/brightness\" method=\"post\" class=\"formbox\">"
 "<p>Brightness:"
-"<input type=\"number\" min=\"0\" max=\"255\" name=\"brightness\"></input>"
+"<input type=\"number\" min=\"0\" max=\"255\" value=\"8\" name=\"brightness\"required></input>"
 "<button type=\"submit\">Set</button></p>"
 "</form>"
 "</body>"
@@ -54,9 +55,11 @@ void handleText() {
   if (!server.hasArg("text") || !server.hasArg("color"))
     return handleFail("BAD ARGS");
   //Serial.println(server.arg("text"));
-  _display->setText(server.arg("text"));
+  if (server.arg("text").length())
+    _display->setText(server.arg("text"));
   //Serial.println(server.arg("color"));
-  _display->setTextHtmlColor(server.arg("color"));
+  if (server.arg("color").length())
+    _display->setTextHtmlColor(server.arg("color"));
   handleRoot();
 }
 
@@ -104,7 +107,7 @@ void exec(void)
       String ipstr = String(_ip[2]) + "." + String(_ip[3]);
       _display->setText(ipstr);
       
-      if (MDNS.begin("esp8266")) {
+      if (MDNS.begin("esp8266", _ip)) {
         Serial.println("MDNS responder started");
       }
 
